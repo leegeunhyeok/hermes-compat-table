b98be0d2d9bf504f62e16362a485ad32b2e270ae
leegeunhyeok <dev.ghlee@gmail.com>
2026-05-19 00:16:51 +0900

    ci: add pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://leegeunhyeok.github.io",
+  base: "/hermes-compat-table",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
